d5edfc7ddea447dedf5490615e51ac3e102cac7a
nanditha1214 <83871379+nanditha1214@users.noreply.github.com>
2025-05-19 09:25:33 +0530

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,5 @@
+api_key = "SECRET" #YOUR API KEY(KEEP IT A SECRET)
+wifi_ssid='NETWORK_SSID' # Network SSID
+wifi_password='NETWORK_PWD'  # Network password(Also secret)
+query_url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + api_key
+#Update the URL if needed
